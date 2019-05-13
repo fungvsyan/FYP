d3fb7061e6c08a9bf64b694d412fb222b082f54b
Alex, Suetyan Mok <43975190+fungvsyan@users.noreply.github.com>
2019-05-13 16:41:54 +0800

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -4,5 +4,6 @@ pip install -r requirements.txt
 
 aws s3api create-bucket --bucket fyp-demo-bucket --region ap-southeast-1 --create-bucket-configuration LocationConstraint=ap-southeast-1
 cp lambda_function/* venv/lib/python3.6/dist-packages
+
 sam package --template-file template.yaml --s3-bucket fyp-demo-bucket --output-template-file package.yaml
 aws cloudformation deploy --stack-name fyp-demo-stack --template-file package.yaml --capabilities CAPABILITY_NAMED_IAM
